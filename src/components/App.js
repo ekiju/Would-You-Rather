9da9729da155a9c24cf844d14b7353adde1bcca9
Kiju Lee <leekijuu@gmail.com>
2018-06-21 12:47:20 -0700

    create private route and redirect to 404 if page doesnt exist

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-
+import PrivateRoute from './PrivateRoute'
 import Login from './Login'
 import Dashboard from './Dashboard'
 import Details from './Details'
 import Leaderboard from './Leaderboard'
 import NewQuestion from './NewQuestion'
-// import Error404 from './Error404'
+import Error404 from './Error404'
 import Nav from './Nav'
 import LoadingBar from 'react-redux-loading'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
@@ -25,14 +25,14 @@ class App extends Component {
           <LoadingBar />
           <div className='container' >
             <Nav />
-            {this.props.loading === true && (
-            <Redirect to='/login' /> )}
-            <Route exact path ='/login' component={Login} />
-            <Route exact path='/' component={Dashboard} />
-            <Route exact path='/question/:questionid' component={Details} />
-            <Route exact path='/add' component={NewQuestion} />
-            <Route exact path='/leaderboard' component={Leaderboard} />
-            
+            <Switch>
+              <Route exact path ='/login' component={Login} />
+              <PrivateRoute exact path='/' component={Dashboard} />
+              <PrivateRoute exact path='/question/:questionid' component={Details} />
+              <PrivateRoute exact path='/add' component={NewQuestion} />
+              <PrivateRoute exact path='/leaderboard' component={Leaderboard} />
+              <Route component={Error404} />
+            </Switch>
           </div>
         </Fragment>
       </Router>
